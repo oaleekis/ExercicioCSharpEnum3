8c351f09a21b0f866351b30598045b7b93b5e35f
Alexsander Marçal da Silva <oalexsanderm@gmail.com>
2023-05-22 20:13:39 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioCSharpEnum3/Entities/Order.cs
@@ -0,0 +1,61 @@
+﻿using ExercicioCSharpEnum3.Entities.Enums;
+using System.Globalization;
+using System.Text;
+
+namespace ExercicioCSharpEnum3.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public Client Client { get; set; }
+
+        public Order() { }
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+        public void RemoveItem(OrderItem item) 
+        {  
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double total = 0.0;
+            foreach (var item in Items)
+            {
+                total += item.SubTotal();
+            }
+
+            return total;
+        }
+
+        public override string ToString()
+        {
+           StringBuilder sb = new StringBuilder();
+            sb.AppendLine("ORDER SUMMARY: ");
+            sb.AppendLine($"Order moment: {Moment.ToString("dd/MM/yyyy HH:mm:ss")}");
+            sb.AppendLine($"Order status: {Status}");
+            sb.AppendLine($"Client: {Client}");
+            sb.AppendLine("Order items: ");
+            foreach (OrderItem item in Items) 
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+
+            return sb.ToString();
+        }
+    }
+
+
+}
---
+++ ExercicioCSharpEnum3/Program.cs
@@ -0,0 +1,50 @@
+﻿using ExercicioCSharpEnum3.Entities;
+using ExercicioCSharpEnum3.Entities.Enums;
+using System.Globalization;
+
+namespace ExercicioCSharpEnum3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Client cliente = new Client(name, email, birthDate);
+
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Console.Write("How many items to this order? ");
+            int qtd = int.Parse(Console.ReadLine());
+
+            DateTime moment = DateTime.Now;
+            Order order = new Order(moment, status, cliente);
+
+            for (int i = 0; i < qtd; i++)
+            {
+                Console.WriteLine($"Enter #{i + 1} item data: ");
+                Console.Write("Product name: ");
+                string nameProduct = Console.ReadLine();
+                Console.Write("Product price: ");
+                double priceProduct = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                Product product = new Product(nameProduct, priceProduct);
+                OrderItem item = new OrderItem(quantity, priceProduct, product);
+
+                order.Items.Add(item);
+
+            }
+
+            Console.WriteLine(order);
+        }
+    }
+}
